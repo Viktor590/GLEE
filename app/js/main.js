ce292876d74f31be44c45a010843d05eb65966e7
Viktor <resident1081994@gmail.com>
2021-03-09 22:37:52 +0300

    added three page

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,16 @@ $(function () {
     autoplaySpeed: 2000
   });
 
+  const mixerProducts = mixitup('.products-item__content-inner', {
+    selectors: {
+      control: '.products-item__btn'
+    }
+  });
+  const mixerDesign = mixitup('.design-inner', {
+    selectors: {
+      control: '.design-filter__btn'
+    }
+  });
+
+
 })
